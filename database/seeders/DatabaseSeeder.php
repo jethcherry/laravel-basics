5bb614016be7d3f025c7207d40dbaaa2958ea1e0
jeth cherry <jethrosumbeiywet@gmail.com.com>
2024-12-31 23:18:48 +0300

    Interacting with database using laravel query builder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Product;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+       $products= Product::factory(50)->create();
     }
 }
